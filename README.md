0fd16c4e32b00b939194eb835e955ab2ba4ffe3d
Tricia Norris <tricia@Tricias-MacBook-Air.local>
2019-03-20 11:06:49 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # apify-scraper
+
+The Apify Scraper crawls and scrapes websites in order to extract information.
+
+Get Started:
+1. After cloning this repository and running `npm install` in the command line to install all necessary node modules, type `npm run start` to run scraper.
+
+1. If successful, you will see the scrape live in Chromium and then be returned to your terminal/editor and the scraped data will appear in the terminal.
--- main.js
+++ main.js
@@ -8,6 +8,14 @@ Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
     await requestQueue.addRequest(new Apify.Request({ url: 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/' }));
     
+    // const requestList = await Apify.openRequestList();
+    // await requestList.addRequest(new Apify.RequestList({ 
+    //     sources: [
+    //     {url: 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/' },
+    //     {url: 'https://www.visithoustontexas.com/events/'}
+    //     ]
+    // }));
+    
 
     // Create crawler.
     const crawler = new Apify.PuppeteerCrawler({
@@ -17,6 +25,7 @@ Apify.main(async () => {
         // If request failes then it's retried 3 times.
         // Parameter page is Puppeteers page object with loaded page.
         handlePageFunction: getEventData,
+        // handlePageFunction: getEventList,
         
 
         // If request failed 4 times then this function is executed.
@@ -35,7 +44,7 @@ Apify.main(async () => {
 const getEventData = async ({ page, request }) => {
 
     // Function to get data from page
-    const title = await page.title();  
+    // const title = await page.title();  
     const event = await page.$('div h1'); 
     const eventHandle = await page.evaluateHandle(body => body.innerHTML, event);
     const date = await page.$('div.dates');
@@ -47,21 +56,26 @@ const getEventData = async ({ page, request }) => {
     const locationHandle = await page.evaluateHandle(body => body.innerHTML, location);
     const address = await page.$('div.adrs');
     const addressHandle = await page.evaluateHandle(body => body.innerHTML, address);
-    const contact = await page.$('div.detail-c2.left div:nth-child(6)'); // #eventDetails_4955259d-7598-4eca-9e80-3e81971a4d2f > div.detail-top > div.detail-c2.left > div:nth-child(6)
-    const contactHandle = await page.evaluateHandle(body => body.innerHTML, contact, remove("strong"));
+    
+    const contact = await page.$('div.detail-c2.left div:nth-child(6)'); 
+    const contactHandle = await page.evaluateHandle(body => body.innerHTML, contact);
+    const phone = await page.$('div.detail-c2.left div:nth-child(7)'); 
+    const phoneHandle = await page.evaluateHandle(body => body.innerHTML, phone);
+    const time = await page.$('div.detail-c2.left div:nth-child(8)'); 
+    const timeHandle = await page.evaluateHandle(body => body.innerHTML, time);    // Admission nth-child(9)  #eventDetails_4955259d-7598-4eca-9e80-3e81971a4d2f > div.detail-top > div.detail-c2.left > div:nth-child(9)
+    // Timestamp    
 
-    // #nav-default > div > div.headerWrapper.theme-default > div.header-bottom > div > div.contentRender.contentRender_5.contentRender_type_widget.contentRender_name_plugins_nav_main > div.navFixedWrapper > div > div > nav > div > div:nth-child(6)
-
-    // Phone nth-child(7) #eventDetails_4955259d-7598-4eca-9e80-3e81971a4d2f > div.detail-top > div.detail-c2.left > div:nth-child(7)
-    // Time(s) nth-child(8) #eventDetails_4955259d-7598-4eca-9e80-3e81971a4d2f > div.detail-top > div.detail-c2.left > div:nth-child(8)
-    // Admission nth-child(9)  #eventDetails_4955259d-7598-4eca-9e80-3e81971a4d2f > div.detail-top > div.detail-c2.left > div:nth-child(9)
-    // Timestamp
-
-    console.log(`URL: ${request.url}`, `Event name: ${eventHandle}`, `Event date: ${dateHandle}`, `Event location: ${locationHandle}`, `Event address: ${addressHandle}`, `Contact: ${contactHandle}`);
-    // console.log(`Event name: ${eventHandle}`, `URL: ${request.url}`, `Event date: ${dateHandle}`, `Event location: ${locationHandle}`, `Event address: ${addressHandle}`);
+    console.log(`URL: ${request.url}` + "\n", `Event name: ${eventHandle}`+ "\n", `Event date: ${dateHandle}`+ "\n", `Event location: ${locationHandle}`+ "\n", `Event address: ${addressHandle}`+ "\n", `Contact: ${contactHandle}`+ "\n", `Phone: ${phoneHandle}`+ "\n", `Time: ${timeHandle}`);
     
     // Log data (util is a tool that nicely formats objects in the console)
-    console.log(util.inspect(title, date, location, address));
+    console.log(util.inspect(eventHandle, false));
+    console.log(util.inspect(dateHandle, false));
+    console.log(util.inspect(locationHandle, false));
 }
 
+// const getEventList= async ({ page }) => {
+    // scrape the event links
+    // const eventLink = await page.$('div.title a');
 
+    // console.log(`Event link: ${eventLink}`);
+// }
